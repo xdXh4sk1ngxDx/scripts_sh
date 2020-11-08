d2fcaba48073d93579b8f98ad7509928bafdce41
xdXh4sk1ngxDx <71937506+xdXh4sk1ngxDx@users.noreply.github.com>
2020-11-08 17:33:02 -0300

    Create parsing.sh
    Listagem de sites vinculados ao atributo href, do código HTML.

---
+++ parsing.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+if [ $1 == ""]
+then
+  echo "instrucao de uso:"
+  echo "ex: $0 site.com.br"
+ else
+  figlet 'parsing'
+  echo
+  
+  wget -q $1
+  mv index.html $1.txt
+  lista_urls=$(cat $1.txt| grep "href" | grep "// | cut -d "/" -f3 | cut -d '"' -f1 | sort -u)
+  
+  for url in $lista_urls
+  do
+  host $url | grep "has address"
+  done
+  
+  rm $1.txt
+fi
